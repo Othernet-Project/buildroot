6a288388f45b9efba9ab2c9f10738ed01e10c7a7
Alex Kaplan <kaplan2539@gmail.com>
2015-09-12 00:51:10 +0000

    chip: working on upload.sh

--- board/nextthing/chip/upload.sh
+++ board/nextthing/chip/upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BUILD_NUMBER=$1
+
 s3cmd put --acl-public --no-guess-mime-type --recursive output/images s3://stak-images/firmware/chip/stable/${BUILD_NUMBER}/ |tee s3cmd_out.txt
 cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >latest
 s3cmd put --acl-public --no-guess-mime-type latest s3://stak-images/firmware/chip/stable/latest
