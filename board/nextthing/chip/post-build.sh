591aede10c3014b06d539116d4c655bec2f40b82
Alex Kaplan <kaplan2539@gmail.com>
2015-09-16 00:41:23 +0000

    added version info to /etc/issue

--- board/nextthing/chip/post-build.sh
+++ board/nextthing/chip/post-build.sh
@@ -3,3 +3,5 @@
 TARGET_DIR=$1
 
 echo "ttyGS0::respawn:/sbin/getty -L ttyGS0 115200 vt100 # gadget serial" >>${TARGET_DIR}/etc/inittab
+
+echo "Welcome to CHIP Buildroot build $(cat build) rev $(cat buildroot_githash)" >${TARGET_DIR}/etc/issue
