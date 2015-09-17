460d5ab1f3e3920664f0b841b50b83bdeab33889
Jason Abele <jason@jasonabeleconsulting.com>
2015-09-17 16:48:08 -0700

    overlay: create overlay and fixup framebuffer console
    Create board specific overlay with inittab and securtty
    Remove inittab modification from post-build.sh
    Enable overlay in chip_defconfig

--- board/nextthing/chip/post-build.sh
+++ board/nextthing/chip/post-build.sh
@@ -2,6 +2,4 @@
 
 TARGET_DIR=$1
 
-echo "ttyGS0::respawn:/sbin/getty -L ttyGS0 115200 vt100 # gadget serial" >>${TARGET_DIR}/etc/inittab
-
 echo "Welcome to CHIP Buildroot build $(cat build) rev $(cat buildroot_githash)" >${TARGET_DIR}/etc/issue
