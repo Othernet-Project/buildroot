60b7a9bfcd8c77c338e7bc913a0d240a1d7b6b2c
Alex Kaplan <kaplan2539@gmail.com>
2015-09-21 14:32:54 -0700

    CHIP postbuild: fixed typo

--- board/nextthing/chip/post-build.sh
+++ board/nextthing/chip/post-build.sh
@@ -3,7 +3,7 @@
 TARGET_DIR=$1
 
 BUILDROOT_GITHASH="$(cat buildroot_githash)"
-BUILDROOT_GITHASH="${BUILD_ROOT_GITHASH:0:8}"
+BUILDROOT_GITHASH="${BUILDROOT_GITHASH:0:8}"
 
 BUILD="$(cat build)"
 
@@ -11,6 +11,8 @@ cat <<EOF >${TARGET_DIR}/etc/issue
 Welcome to CHIP Buildroot build ${BUILD} rev ${BUILDROOT_GITHASH}
 
 CHIP Buildroot contains various open source software.
+
 The source code can be downloaded from:
 http://opensource.nextthing.co/chip/buildroot/stable/${BUILD}/build${BUILD}.tar.gz
+
 EOF
