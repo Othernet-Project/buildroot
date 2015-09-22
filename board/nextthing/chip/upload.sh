aba9696f730c8691b5a9a68f05ba1a86b798f07a
Alex Kaplan <kaplan2539@gmail.com>
2015-09-22 16:24:57 -0700

    also upload file containing build number to s3

--- board/nextthing/chip/upload.sh
+++ board/nextthing/chip/upload.sh
@@ -4,6 +4,10 @@ BUILD_NUMBER=$1
 
 if s3cmd put --acl-public --no-guess-mime-type --recursive output/images s3://stak-images/firmware/chip/stable/${BUILD_NUMBER}/ |tee s3cmd_out.txt
 then
+  echo "${BUILD_NUMBER}" >upload_buildno
+  
+  s3cmd put --acl-public --no-guess-mime-type upload_buildno s3://stak-images/firmware/chip/stable/${BUILD_NUMBER}/build 
+
 	cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >latest
 	s3cmd put --acl-public --no-guess-mime-type latest s3://stak-images/firmware/chip/stable/latest
 fi
