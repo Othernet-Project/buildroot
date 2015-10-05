e12985c043651029555d0a3f799edfb5a4a6450d
Wynter Woods <zerotri@gmail.com>
2015-10-05 10:33:23 -0700

    We should also push updates to opensource url

--- board/nextthing/chip/upload.sh
+++ board/nextthing/chip/upload.sh
@@ -2,7 +2,8 @@
 
 BUILD_NUMBER=$1
 
-if s3cmd put --acl-public --no-guess-mime-type --recursive output/images s3://stak-images/firmware/chip/stable/${BUILD_NUMBER}/ |tee s3cmd_out.txt
+if s3cmd put --acl-public --no-guess-mime-type --recursive output/images s3://stak-images/firmware/chip/stable/${BUILD_NUMBER}/ |tee s3cmd_out.txt &&
+if s3cmd put --acl-public --no-guess-mime-type --recursive output/images s3://opensource.nextthing.co/chip/buildroot/stable/${BUILD_NUMBER}/ |tee s3cmd_os_out.txt
 then
   echo "${BUILD_NUMBER}" >upload_buildno
   
@@ -10,4 +11,7 @@ then
 
 	cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >latest
 	s3cmd put --acl-public --no-guess-mime-type latest s3://stak-images/firmware/chip/stable/latest
+	
+	cat s3cmd_os_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >os_latest
+	s3cmd put --acl-public --no-guess-mime-type --add-header="x-amz-website-redirect-location:/chip/buildroot/stable/${BUILD_NUMBER}/rootfs.ubi" os_latest s3://opensource.nextthing.co/chip/buildroot/stable/latest
 fi
