303cad0d92a9c36438152e64e3619cd45333e0dd
Alex Kaplan <kaplan2539@gmail.com>
2015-10-13 19:28:07 -0700

    board/nextthing/chip: updated the upload and postbuild scripts

--- board/nextthing/chip/post-build.sh
+++ board/nextthing/chip/post-build.sh
@@ -2,17 +2,20 @@
 
 TARGET_DIR=$1
 
+BRANCH=$(git symbolic-ref -q HEAD)
+BRANCH=${BRANCH##refs/heads/chip/}
+
 BUILDROOT_GITHASH="$(cat buildroot_githash)"
 BUILDROOT_GITHASH="${BUILDROOT_GITHASH:0:8}"
 
 BUILD="$(cat build)"
 
 cat <<EOF >${TARGET_DIR}/etc/issue
-Welcome to CHIP Buildroot build ${BUILD} rev ${BUILDROOT_GITHASH}
+Welcome to CHIP Buildroot-${BRANCH} build ${BUILD} rev ${BUILDROOT_GITHASH}
 
 CHIP Buildroot contains various open source software.
 
 The source code can be downloaded from:
-http://opensource.nextthing.co/chip/buildroot/stable/${BUILD}/build${BUILD}.tar.gz
+http://opensource.nextthing.co/chip/buildroot/${BRANCH}/${BUILD}/build${BUILD}.tar.gz
 
 EOF
--- board/nextthing/chip/upload.sh
+++ board/nextthing/chip/upload.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+BRANCH=$(git symbolic-ref -q HEAD)
+BRANCH=${BRANCH##refs/heads/chip/}
+
 BUILD_NUMBER=$1
-S3_DEST=s3://opensource.nextthing.co/chip/buildroot/bugfix
+S3_DEST=s3://opensource.nextthing.co/chip/buildroot/${BRANCH}
+# IMPORTANT: in order to have the md5sum as an etag one has to use --disable-multipart
+# also see: http://docs.aws.amazon.com/AmazonS3/latest/API/RESTCommonResponseHeaders.html
 S3_OPTIONS="--acl-public --no-guess-mime-type --disable-multipart"
 
 if s3cmd put ${S3_OPTIONS} --recursive output/images ${S3_DEST}/${BUILD_NUMBER}/ |tee s3cmd_out.txt
@@ -10,9 +15,9 @@ then
   
   s3cmd put ${S3_OPTIONS} upload_buildno ${S3_DEST}/${BUILD_NUMBER}/build 
 
-	cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >latest
+  cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*\(http:\/\/.*\/[0-9]\+\)\/.*/\1/;' >latest
 	s3cmd put ${S3_OPTIONS} latest ${S3_DEST}/latest
 	
 	#cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >os_latest
-	#s3cmd put ${S3_OPTIONS} --add-header="x-amz-website-redirect-location:/chip/buildroot/stable/${BUILD_NUMBER}/rootfs.ubi" os_latest s3://opensource.nextthing.co/chip/buildroot/stable/latest
+	#s3cmd put ${S3_OPTIONS} --add-header="x-amz-website-redirect-location:/chip/buildroot/${BRANCH}/${BUILD_NUMBER}/rootfs.ubi" os_latest s3://opensource.nextthing.co/chip/buildroot/stable/latest
 fi
