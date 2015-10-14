3eee6457abca52997bb7456c441414f938a7dabc
Alex Kaplan <kaplan2539@gmail.com>
2015-10-13 19:08:28 -0700

    board/nextthing/chip: disable s3cmd multi part upload

--- board/nextthing/chip/upload.sh
+++ board/nextthing/chip/upload.sh
@@ -2,16 +2,17 @@
 
 BUILD_NUMBER=$1
 S3_DEST=s3://opensource.nextthing.co/chip/buildroot/bugfix
+S3_OPTIONS="--acl-public --no-guess-mime-type --disable-multipart"
 
-if s3cmd put --acl-public --no-guess-mime-type --recursive output/images ${S3_DEST}/${BUILD_NUMBER}/ |tee s3cmd_out.txt
+if s3cmd put ${S3_OPTIONS} --recursive output/images ${S3_DEST}/${BUILD_NUMBER}/ |tee s3cmd_out.txt
 then
   echo "${BUILD_NUMBER}" >upload_buildno
   
-  s3cmd put --acl-public --no-guess-mime-type upload_buildno ${S3_DEST}/${BUILD_NUMBER}/build 
+  s3cmd put ${S3_OPTIONS} upload_buildno ${S3_DEST}/${BUILD_NUMBER}/build 
 
 	cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >latest
-	s3cmd put --acl-public --no-guess-mime-type latest ${S3_DEST}/latest
+	s3cmd put ${S3_OPTIONS} latest ${S3_DEST}/latest
 	
 	#cat s3cmd_out.txt |grep Public |head -n1 |sed -e 's/.*http:/http:/; s/\(http:.*\/\).*\/.*/\1/' >os_latest
-	#s3cmd put --acl-public --no-guess-mime-type --add-header="x-amz-website-redirect-location:/chip/buildroot/stable/${BUILD_NUMBER}/rootfs.ubi" os_latest s3://opensource.nextthing.co/chip/buildroot/stable/latest
+	#s3cmd put ${S3_OPTIONS} --add-header="x-amz-website-redirect-location:/chip/buildroot/stable/${BUILD_NUMBER}/rootfs.ubi" os_latest s3://opensource.nextthing.co/chip/buildroot/stable/latest
 fi
