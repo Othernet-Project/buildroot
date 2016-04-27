f33e774c078e0f232699bbf76feeae8ee0ede093
Branko Vukelic <branko@outernet.is>
2016-04-27 03:15:02 +0200

    Allow using separate cpio archive for initramfs
    This patch adds an option to initramfs filesystem target that allows the use of
    a separate user-generated cpio archive in conjunction with a root filesystem
    image.
    
    The new option is called 'Initramfs cpio archive' and allows the choice between
    two options:
    
    - 'generate from root filesystem' is the default and behaves the same way as
      before: rootfs.cpio is generated by depending on cpio target.
    
    - 'generate separately' creates a dummy rootfs.cpio target that is rigged to
      fail if there is no rootfs.cpio file in the images/ directory but otherwise
      does not try to generate one.
    
    The second option is meant to be used with a post-build hook that creates a
    custom rootfs.cpio.
    
    Selection of these two modes is done using `choice` to allow for future
    expansion as well as to avoid ambiguity about what 'the other' option would be.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- fs/initramfs/initramfs.mk
+++ fs/initramfs/initramfs.mk
@@ -5,7 +5,9 @@
 #
 ################################################################################
 
+ifeq ($(BR2_INITRAMFS_USE_ROOTFS),y)
 ROOTFS_INITRAMFS_DEPENDENCIES += rootfs-cpio
+endif
 
 ROOTFS_INITRAMFS_POST_TARGETS += linux-rebuild-with-initramfs
 
@@ -18,6 +20,22 @@ rootfs-initramfs-show-depends:
 	@echo $(ROOTFS_INITRAMFS_DEPENDENCIES)
 
 .PHONY: rootfs-initramfs rootfs-initramfs-show-depends
+
+
+ifeq ($(BR2_INITRAMFS_USE_OTHER),y)
+ifndef $(BR_ROOTFS_CPIO)
+
+# Define a dummy target for rootfs.cpio so that 
+# linux-rebuild-with-iniramfs dependency is 
+# satisfied. A test is also preformed so that the
+# build does not yield unintended results.
+$(BINARIES_DIR)/rootfs.cpio: target-finalize
+	@$(call MESSAGE,"Using existing rootfs.cpio")
+	@test -f $@ || echo "ERROR: '$(BINARIES_DIR)/rootfs.cpio' was not found"
+
+endif
+endif
+
 
 ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
 TARGETS_ROOTFS += rootfs-initramfs
