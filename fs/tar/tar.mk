a08389c9e23442554392470d9529e49b0366aa8d
abhishek <abhishek@outernet.is>
2016-11-11 21:30:31 +0530

    use KBUILD standard for time stamp

--- fs/tar/tar.mk
+++ fs/tar/tar.mk
@@ -7,7 +7,7 @@
 TAR_OPTS := $(call qstrip,$(BR2_TARGET_ROOTFS_TAR_OPTIONS))
 
 define ROOTFS_TAR_CMD
-    (cd $(TARGET_DIR) ; find . -print0) | sort -z | tar -c$(TAR_OPTS)f $@ --numeric-owner -C $(TARGET_DIR) --mtime='2016-01-01' --no-recursion --null -T -
+    (cd $(TARGET_DIR) ; find . -print0) | sort -z | tar -c$(TAR_OPTS)f $@ --numeric-owner -C $(TARGET_DIR) --mtime="$(KBUILD_BUILD_TIMESTAMP)" --no-recursion --null -T -
 endef
 
 $(eval $(call ROOTFS_TARGET,tar))
