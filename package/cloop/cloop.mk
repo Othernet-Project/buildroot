ad80a4b25e5752855485c31ce0a0a48a677423e3
abhishek <abhishek@outernet.is>
2016-12-27 01:50:14 +0530

    change to updated cloop code (doesn't build yet)

--- package/cloop/cloop.mk
+++ package/cloop/cloop.mk
@@ -4,13 +4,26 @@
 #
 ################################################################################
 
-CLOOP_VERSION = 2.634-1
-CLOOP_SOURCE = cloop_$(CLOOP_VERSION).tar.gz
-CLOOP_SITE = http://debian-knoppix.alioth.debian.org/packages/sources/cloop
+CLOOP_VERSION = 4.3-2-br
+CLOOP_SITE = https://github.com/Outernet-Project/cloop
+CLOOP_SITE_METHOD = git
 CLOOP_LICENSE = GPLv2 (module), GPLv2+ (advancecomp)
 CLOOP_LICENSE_FILES = README advancecomp-1.15/COPYING
 
 HOST_CLOOP_DEPENDENCIES = host-zlib
+CLOOP_DEPENDENCIES = zlib linux
+
+define CLOOP_BUILD_CMDS
+    $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNEL_DIR=$(LINUX_DIR)
+endef
+
+define CLOOP_INSTALL_TARGET_CMDS
+    $(INSTALL) -p -m 644 $(@D)/cloop.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/
+    depmod -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 $(@D)/create_compressed_fs $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 $(@D)/extract_compressed_fs $(TARGET_DIR)/usr/bin
+endef
 
 define HOST_CLOOP_BUILD_CMDS
 	$(HOST_CONFIGURE_OPTS) $(MAKE1) -C $(@D) APPSONLY=yes
@@ -22,4 +35,5 @@ define HOST_CLOOP_INSTALL_CMDS
 	$(INSTALL) -m 755 $(@D)/extract_compressed_fs $(HOST_DIR)/usr/bin
 endef
 
+$(eval $(generic-package))
 $(eval $(host-generic-package))
