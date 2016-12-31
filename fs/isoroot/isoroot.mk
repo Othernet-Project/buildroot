b4b7a571a6aa1595e363eaf0d3e83a1481b316b2
abhishek <abhishek@outernet.is>
2016-12-31 16:53:34 +0530

    quiet down isoroot generation

--- fs/isoroot/isoroot.mk
+++ fs/isoroot/isoroot.mk
@@ -7,7 +7,7 @@
 ROOTFS_ISOROOT_DEPENDENCIES = host-cdrkit
 
 define ROOTFS_ISOROOT_CMD
-	$(HOST_DIR)/usr/bin/genisoimage -R -iso-level 4 $(TARGET_DIR) > $@
+	$(HOST_DIR)/usr/bin/genisoimage -quiet -R -iso-level 4 $(TARGET_DIR) > $@
 endef
 
 $(eval $(call ROOTFS_TARGET,isoroot))
