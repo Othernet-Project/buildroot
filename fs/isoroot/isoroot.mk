fd63e2b361dc88f1a7b0101280184826a592c310
abhishek <abhishek@outernet.is>
2017-02-04 23:30:06 +0530

    conditionally use mkimagesdir.sh from sd-image package or ubi-image package

--- fs/isoroot/isoroot.mk
+++ fs/isoroot/isoroot.mk
@@ -6,9 +6,18 @@
 
 ROOTFS_ISOROOT_DEPENDENCIES = host-cdrkit
 
+ifeq ($(BR2_PACKAGE_SD_IMAGE),y)
+define ROOTFS_ISOROOT_CMD
+    $(SD_IMAGE_SITE)/mkimagesdir.sh ; \
+	$(HOST_DIR)/usr/bin/genisoimage -quiet -R -iso-level 4 $(TARGET_DIR) $(BINARIES_DIR)/images > $@
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_UBI_IMAGE),y)
 define ROOTFS_ISOROOT_CMD
     $(UBI_IMAGE_SITE)/mkimagesdir.sh ; \
 	$(HOST_DIR)/usr/bin/genisoimage -quiet -R -iso-level 4 $(TARGET_DIR) $(BINARIES_DIR)/images > $@
 endef
+endif
 
 $(eval $(call ROOTFS_TARGET,isoroot))
